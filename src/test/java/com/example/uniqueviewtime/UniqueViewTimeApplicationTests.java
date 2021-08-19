3985725d664bb444be72f4d16d547602daf16322
Phillip Kim <Phillip.Kim.3112@gmail.com>
2021-08-19 14:49:55 -0500

    attempted single test spec

--- src/test/java/com/example/uniqueviewtime/UniqueViewTimeApplicationTests.java
+++ src/test/java/com/example/uniqueviewtime/UniqueViewTimeApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.uniqueviewtime;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UniqueViewTimeApplicationTests {
 
+	UniqueViewTime uVTTest = new UniqueViewTime();
+
 	@Test
-	void contextLoads() {
+	void testingUVT() {
+
+		// given
+		ViewFragment firstFragment = new ViewFragment(0.0, 1.1);
+		ViewFragment secondFragment = new ViewFragment(2.5, 5.6);
+
+		ViewFragment arr[] = new ViewFragment[2];
+		arr[0] = firstFragment;
+		arr[1] = secondFragment;
+
+		// when
+		Double uVTResult = uVTTest.uniqueViewTimeCalculator(arr);
+
+		// Expect
+		assertEquals(4.2, uVTResult);
+
 	}
 
 }
