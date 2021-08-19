acfc9d0043c759185e5b607e2d1bfc962c49bcc9
Phillip Kim <Phillip.Kim.3112@gmail.com>
2021-08-19 11:51:54 -0500

    first commit

---
+++ src/main/java/com/example/uniqueviewtime/UniqueViewTimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.uniqueviewtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniqueViewTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniqueViewTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uniqueviewtime/UniqueViewTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uniqueviewtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniqueViewTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
